9eec2314f9c072022687cb26b0be439af0dc034c
Militsa Tsvetkova <militsa.tsvetkova@officernd.com>
2026-01-15 15:37:12 +0200

    required and optional inputs

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -8,8 +8,14 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.css',
 })
 export class User {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({
+    required: true,
+  })
+  avatar!: string;
+  @Input({
+    required: true,
+  })
+  name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
